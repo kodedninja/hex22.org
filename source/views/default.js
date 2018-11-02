f0394795c8e9221cf8334fd0f4b99f94207edf3b
Hunor Karamán <hunix94@gmail.com>
2018-11-02 15:53:35 +0100

    super restructure

--- source/views/default.js
+++ source/views/default.js
@@ -6,7 +6,19 @@ module.exports = view
 function view (state, emit) {
 	return html`
 		<div class="1 db">
-			${format(state.page().v('text'))}
+			${title()}
+			<div class="1 db">
+				${format(state.page().v('text'))}
+			</div>
 		</div>
 	`
+
+	function title() {
+		if (state.page().v('banner') == true) return html`
+			<div class="1 db fwb f1 mb2 lh1">
+				${state.page().v('title')}
+			</div>
+		`
+		return html``
+	}
 }
