532ad4685816baa16be827553b05d368486645d8
Hunor Karaman <karamanhunor@protonmail.com>
2019-03-09 09:04:51 +0100

    not found

--- source/views/notfound.js
+++ source/views/notfound.js
@@ -4,9 +4,14 @@ var format = require('../components/format')
 module.exports = view
 
 function view (state, emit) {
-	return html`
-		<div class="1 db">
-	  		Not Found
-		</div>
+  return html`
+    <div class="1 db">
+      <div class="tac">
+				<div class="mb0-5">There's nothing here.</div>
+				<div>
+					<a href="/">Back to the Index</a>
+				</div>
+      </div>
+    </div>
   `
 }
