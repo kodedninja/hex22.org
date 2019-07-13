4e941a6c8e834dcece26b903a561be7efd91650a
hunor <hunor.karaman@geogebra.org>
2019-07-13 13:21:53 +0200

    final changes to new stack

--- source/plugins/scroll.js
+++ source/plugins/scroll.js
@@ -4,4 +4,8 @@ function scroll (state, emitter) {
   emitter.on(state.events.NAVIGATE, function () {
     window.scrollTo(0, 0)
   })
+
+  emitter.on(state.events.POPSTATE, function () {
+    window.scrollTo(0, 0)
+  })
 }
