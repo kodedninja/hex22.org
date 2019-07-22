fe40cccb96f410c8988ffcf2c2551a4067cc3c12
hunor <hunor.karaman@geogebra.org>
2019-07-22 21:58:20 +0200

    added galleries

--- source/components/content.js
+++ source/components/content.js
@@ -21,6 +21,24 @@ module.exports = class Content extends Component {
 
 	formatimages() {
 		var element = this.element
+
+    var galleries = [...element.querySelectorAll('.gallery')]
+    galleries.forEach(function (group) {
+      group.classList.add('clear', '1', 'db')
+
+      var images = [...group.children]
+      images.map(function (image) {
+        var alt = image.getAttribute('alt') || ''
+        var column = html`
+          <div class="dib fl tile 1/${group.children.length}">
+            ${image}
+            <div class="tac f2">${alt}</div>
+          </div>
+        `
+        group.appendChild(column)
+      })
+    })
+		
 		var images = [...element.querySelectorAll('img')].forEach(function (image) {
 			if (image.parentNode.nodeName !== 'A') {
 				mediumZoom(image, {
@@ -40,7 +58,7 @@ module.exports = class Content extends Component {
 
 	afterupdate(el) {
 		if (typeof window !== 'undefined') {
-			this.formatimages()
+		  this.formatimages()
 		}
 	}
 
