df7d8f6347f8aa9284e2c43068c7feb2cf5d9202
hunor <hunor.karaman@geogebra.org>
2022-09-01 08:27:49 +0200

    xund

--- source/views/home.js
+++ source/views/home.js
@@ -10,17 +10,6 @@ function home (state, emit) {
 			  <div class="mw400 mxa">
   				${format(state.page().v('text'))}
 			  </div>
-				<div class="w-1 db clear-float my1">
-          <div class="w-50 m-w-100 pr0-2 m-px0 m-mb0-5 fl dib">
-            ${format(state.page().v('left'))}
-          </div>
-          <div class="w-50 m-w-100 pl0-2 m-px0 fl dib">
-            ${format(state.page().v('right'))}
-          </div>
-				</div>
-				<div class="mw400 mxa">
-  				${format(state.page().v('bottom'))}
-				</div>
 			</div>
 		</div>
 	`
